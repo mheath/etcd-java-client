f8ffb52db4c18bcfb75b02dc66caa516a5654819
Mike Heath <elcapo@gmail.com>
2014-10-23 13:43:42 -0600

    Removed email addresses in @author tags.

--- src/main/java/etcd/client/EtcdFuture.java
+++ src/main/java/etcd/client/EtcdFuture.java
@@ -2,8 +2,5 @@ package etcd.client;
 
 import io.netty.util.concurrent.Future;
 
-/**
- * @author Mike Heath <elcapo@gmail.com>
- */
 public interface EtcdFuture extends Future<Result> {
 }
--- src/main/java/etcd/client/EtcdListener.java
+++ src/main/java/etcd/client/EtcdListener.java
@@ -2,8 +2,5 @@ package etcd.client;
 
 import io.netty.util.concurrent.GenericFutureListener;
 
-/**
- * @author Mike Heath <elcapo@gmail.com>
- */
 public interface EtcdListener extends GenericFutureListener<EtcdFuture> {
 }
